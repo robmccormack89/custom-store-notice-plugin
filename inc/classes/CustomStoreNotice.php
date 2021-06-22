d23b907e2247f1b7bf09defc753290b2edfb36db
robmccormack89 <robertmccormack147@gmail.com>
2021-06-22 17:20:43 +0100

    rename method

--- inc/classes/CustomStoreNotice.php
+++ inc/classes/CustomStoreNotice.php
@@ -10,14 +10,14 @@ class CustomStoreNotice {
 
   public function __construct() {
     // enqueue plugin assets
-    add_action('wp_enqueue_scripts', array($this, 'customer_store_notice_assets'));
+    add_action('wp_enqueue_scripts', array($this, 'custom_store_notice_assets'));
     
     // after plugins are loaded, do our stuff. this is so other plugins have a chance to do their stuff first
     add_action('plugins_loaded', array($this, 'remove_demo_store_notice'));
     add_action('rmcc_before_header', 'add_custom_demo_store_notice', 10);
   }
   
-  public function customer_store_notice_assets() {
+  public function custom_store_notice_assets() {
     wp_enqueue_style(
       'custom-store-notice',
       CUSTOM_STORE_NOTICE_URL . 'public/css/custom-store-notice.css'
